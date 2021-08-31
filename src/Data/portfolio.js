642af3fff905ebc4def85ec86bce1de89b5a2742
burnett-micheal <burnett.micheal.business@gmail.com>
2021-08-31 17:02:30 -0500

    changed testimonial names to fake

--- src/Data/portfolio.js
+++ src/Data/portfolio.js
@@ -1,12 +1,12 @@
 const testimonials = 
 [
     {
-    name: "Branndon Delk", 
+    name: "Example Testimonial 0", 
     company: "Host A Fan", 
     testimonial: `Micheals work and team spirit at Host A Fan always surprised me. Oftentimes he would document practices that he found useful and actively share them with the team. He also helped organize the client presentations via workflowy and regular team meetings. While I was struggling with X problem he personally hopped on call and helped me sort it out. He was a great co-worker and I enjoyed learning and developing alongside him.`
     },
     {
-     name: "Darrel Baerwaldt",
+     name: "Example Testimonial 1",
      company: "Host A Fan",
      testimonial: `Micheals work and team spirit at Host A Fan always surprised me. Oftentimes he would document practices that he found useful and actively share them with the team. He also helped organize the client presentations via workflowy and regular team meetings. While I was struggling with X problem he personally hopped on call and helped me sort it out. He was a great co-worker and I enjoyed learning and developing alongside him.` 
     }
